b84ee021c60aeaa274e7f5d16f221df2dd00a171
gpsub <giriprasad590@gmail.com>
2022-02-25 17:56:27 +0400

    added firebase hosting

--- src/Post.js
+++ src/Post.js
@@ -7,7 +7,7 @@ function Post({key,name,description,message,photoUrl}) {
     return (
         <div className='post'>
             <div className="post_header">
-            <Avatar className="headerOption__icon" src={photoUrl}>{description[0]}</Avatar>
+            <Avatar className="headerOption__icon" src={photoUrl}>{name[0]}</Avatar>
                 <div className="post_info">
                     <h2>{name}</h2>
                     <p>{description}</p>
